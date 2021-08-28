f09b51f3400b068c890f57a5334cccb60a7db434
Cesar Puentes <caps2695@gmail.com>
2021-08-28 11:57:34 -0500

    🚧 created my first service

---
+++ src/service/products/products.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  private counterId = 1;
+
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Propduct 1',
+      description: 'bla bla',
+      price: 1234,
+      stock: 100,
+      image: '',
+    },
+  ];
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  create(payload: Product) {
+    this.counterId += 1;
+    const newProduct = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+}
