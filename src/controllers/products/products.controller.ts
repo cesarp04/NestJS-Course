4cf51c414834d787f593cfe7c477c7b84bcc4a22
Cesar Puentes <caps2695@gmail.com>
2021-08-28 12:58:51 -0500

    🚧 error handling

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,20 +1,29 @@
-import { Controller, Get, Param, Query, Post, Body, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from 'src/service/products/products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private productService: ProductsService) {}
+  constructor(private productsService: ProductsService) {}
 
   @Get()
   get_products(@Query('limit') limit = 100, @Query('offset') offset = 2) {
     // return `products limit: ${limit} and offset: ${offset}`;
-    return this.productService.findAll();
+    return this.productsService.findAll();
   }
 
   @Get(':id')
   getProduct(@Param('id') id: string) {
     // return `Product with id:${id}`;
-    return this.productService.findOne(+id);
+    return this.productsService.findOne(+id);
   }
 
   @Post()
@@ -23,11 +32,16 @@ export class ProductsController {
     //   message: 'Product created',
     //   payload,
     // };
-    return this.productService.create(payload);
+    return this.productsService.create(payload);
   }
 
   @Put(':id')
   updateProduct(@Param('id') id: number, @Body() payload: any) {
-    return this.productService.update(id, payload);
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
   }
 }
--- src/service/products/products.service.ts
+++ src/service/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from 'src/entities/product.entity';
 
 @Injectable()
@@ -21,7 +21,11 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return this.products.find((item) => item.id === id);
+    const product = this.products.find((item) => item.id === id);
+    if (!product) {
+      throw new NotFoundException(`Product with ${id} not found`);
+    }
+    return product;
   }
 
   create(payload: Product) {
@@ -42,5 +46,14 @@ export class ProductsService {
       return this.products[index];
     }
     return null;
+  }
+
+  remove(id: number) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Product with ${id} not found`);
+    }
+    this.products.splice(index, 1);
+    return true;
   }
 }
