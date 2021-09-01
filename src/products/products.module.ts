ffa5d5f8c6b75744920768c0ac145964c9719af2
Cesar Puentes <caps2695@gmail.com>
2021-09-01 00:19:42 -0500

    🚧 Interaction between modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 
-import { ProductsController } from './controllers/products/products.controller';
+import { BrandsController } from './controllers/brands/brands.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
+import { ProductsController } from './controllers/products/products.controller';
+import { BrandsService } from './service/brands/brands.service';
+import { CategoriesService } from './service/categories/categories.service';
+
 import { ProductsService } from './service/products/products.service';
 
 @Module({
-  controllers: [ProductsController, CategoriesController],
-  providers: [ProductsService],
+  controllers: [ProductsController, CategoriesController, BrandsController],
+  providers: [ProductsService, BrandsService, CategoriesService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { CustomerController } from './controllers/customers/customers.controller';
+import { UsersController } from './controllers/users/users.controller';
+import { CustomersService } from './services/customers/customers.service';
+import { UsersService } from './services/users/users.service';
+
+import { ProductsModule } from '../products/products.module';
+
+@Module({
+  imports: [ProductsModule],
+  controllers: [CustomerController, UsersController],
+  providers: [CustomersService, UsersService],
+})
 export class UsersModule {}
